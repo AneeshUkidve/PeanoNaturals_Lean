f6d8470d62d84a3ef603571244e2ec25b068213e
AneeshUkidve <aneeshukidve@gmail.com>
2024-04-12 21:04:42 +0530

    Defining multiplaction and inverse
    Proves more properties of addition
    Defines additive inverse function and proves some elementary results.
    Defines multiplication and proves some basic properties

--- FirstProject/Integers.lean
+++ FirstProject/Integers.lean
@@ -247,3 +247,131 @@ theorem add_comm (a b : Integer) : a + b = b + a := by
 
   rename_i b
   simp [add, Natural.add_comm]
+
+theorem add_pred (a b : Integer) : a + pred b = pred (a + b) := by
+  let c := pred a
+  have h : c = pred a := by rfl
+  have hac : a = succ c := by exact pred_to_succ c a h
+  rw [hac, succ_add, succ_add, pred_succ, ←add_succ, succ_pred]
+
+theorem pred_add (a b : Integer) : pred a + b = pred (a + b) := by
+  rw [add_comm, add_pred, add_comm]
+
+theorem add_assoc (a b c : Integer) : a + b + c = a + (b + c) := by
+  cases a
+  rename_i a
+  induction a with
+  |zero =>
+    rw [←zero, zero_add, zero_add]
+  |succ d hd =>
+    calc
+      _ = succ (nat_to_pos d) + b + c := by simp [succ]
+      _ = succ (nat_to_pos d + b + c) := by simp [succ_add]
+      _ = succ (nat_to_pos d + (b + c)) := by rw [hd]
+      _ = succ (nat_to_pos d) + (b + c) := by simp [succ_add]
+      _ = nat_to_pos (Natural.succ d) + (b + c) := by simp [succ]
+
+  rename_i a
+  induction a with
+  |zero =>
+    rewrite [add_comm (nat_to_neg Natural.zero), add_comm (nat_to_neg Natural.zero)]
+    rw [←pred_eq_add_neg_one, ←pred_eq_add_neg_one, pred_add]
+  |succ d hd =>
+    calc
+      _ = pred (nat_to_neg d) + b + c := by simp [pred]
+      _ = pred (nat_to_neg d) + (b + c) := by rw [pred_add, pred_add, hd, ←pred_add]
+      _ = nat_to_neg (Natural.succ d) + (b + c) := by simp [pred]
+
+def inverse : Integer → Integer
+  |nat_to_pos a => succ (nat_to_neg a)
+  |nat_to_neg a => succ (nat_to_pos a)
+
+theorem inv_zero : inverse zero = zero := by rfl
+
+theorem add_inverse_1 (a : Natural) : nat_to_pos a + inverse (nat_to_pos a) = zero := by
+  induction a with
+  |zero =>
+    rfl
+  |succ d hd =>
+    calc
+      _ = succ (nat_to_pos d) + succ (nat_to_neg (Natural.succ d)) := by simp [succ, inverse]
+      _ = succ (nat_to_pos d) + succ (pred (nat_to_neg d)) := by simp [pred]
+      _ = succ (nat_to_pos d + pred (succ (nat_to_neg d))) := by rw [succ_pred, pred_succ, succ_add]
+      _ = succ (pred (nat_to_pos d + inverse (nat_to_pos d))) := by simp [inverse, add_pred]
+      _ = zero := by rw [hd, succ_pred]
+
+theorem add_inverse_2 (a : Natural) : nat_to_neg a + inverse (nat_to_neg a) = zero := by
+  simp [inverse, add_succ]
+  rw [add_comm, ←add_succ]
+  have h : succ (nat_to_neg a) = inverse (nat_to_pos a) := by rfl
+  rw [h]
+  exact add_inverse_1 a
+
+theorem add_inv (a : Integer) : a + inverse a = zero := by
+  cases a
+  rename_i a
+  exact add_inverse_1 a
+  rename_i a
+  exact add_inverse_2 a
+
+theorem inv_add (a : Integer) : inverse a + a = zero := by
+  rw [add_comm, add_inv]
+
+def mul : Integer → Integer → Integer
+  |nat_to_pos a, nat_to_pos b                 => nat_to_pos (a * b)
+  |nat_to_neg _, nat_to_pos Natural.zero      => zero
+  |nat_to_neg a, nat_to_pos (Natural.succ b)  => mul (nat_to_neg a) (nat_to_pos b) + nat_to_neg a
+
+  |nat_to_pos a, nat_to_neg Natural.zero      => succ (nat_to_neg a)
+  |nat_to_pos a, nat_to_neg (Natural.succ b)  => mul (nat_to_pos a) (nat_to_neg b) + succ (nat_to_neg a)
+
+  |nat_to_neg a, nat_to_neg b                 =>nat_to_pos (Natural.succ a * Natural.succ b)
+
+infixl:65 " * " => mul
+
+theorem mul_zero (a : Integer) : a * zero = zero := by cases a; rfl; rfl
+
+theorem zero_mul (a : Integer) : zero * a = zero := by
+  cases a
+  simp [zero, mul, Natural.zero_mul]
+
+  rename_i a
+  induction a with
+  |zero =>
+    rfl
+  |succ d hd =>
+    simp [zero, mul]
+    rw [←zero, hd]
+    simp [succ, zero_add]
+
+theorem mul_one (a : Integer) : a * one = a := by
+  cases a
+  simp [one, zero, succ, mul]
+  rw [←Natural.one, Natural.mul_one]
+
+  rename_i a
+  simp [one, zero, succ, mul]
+  rw [←zero, zero_add]
+
+theorem one_mul (a : Integer) : one * a = a := by
+  cases a
+  simp [one, zero, succ, mul]
+  rw [←Natural.one, Natural.one_mul]
+
+  rename_i a
+  simp [one, zero, succ]
+  cases a
+  rfl
+  simp [mul]
+  rename_i a
+  calc
+    _ = succ (nat_to_pos Natural.zero) * nat_to_neg a + succ (pred (nat_to_neg Natural.zero)) := by simp [succ, pred]
+    _ = nat_to_neg a + nat_to_neg Natural.zero := by rw [←zero, ←one, one_mul, succ_pred]
+    _ = pred (nat_to_neg a) := by rw [pred_eq_add_neg_one]
+    _ = nat_to_neg (Natural.succ a) := by simp [pred]
+
+def neg_one := inverse one
+
+theorem neg_one_eq_neg_zero : neg_one = nat_to_neg Natural.zero := by rfl
+
+theorem inv_eq_mul_neg_one (a : Integer) : inverse a = a * neg_one := sorry
